e9bdacee4dc0a9acbf3f66ad38c99785871a8021
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-05-29 14:50:17 +0530

    Created a basic implementation of the Popup

---
+++ ReusablePopup/ReusablePopup/ReusablePopupViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ReusablePopupViewController.swift
+//  ReusablePopup
+//
+//  Created by bhavesh on 29/05/21.
+//  Copyright © 2021 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+class ReusablePopupViewController: UIViewController {
+
+    //MARK:-IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+
+
+    //MARK:-static properteis
+    static let storyboardIdentifier = "ReusablePopupViewController"
+
+    //MARK:-Member properties
+    var popuptitle: String!
+    var message: String!
+    var actionTitle: String!
+    var action: (() -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = popuptitle
+        messageLabel.text = message
+        actionButton.setTitle(actionTitle, for: .normal)
+
+    }
+
+    @IBAction func actionButtonClicked(_ sender: UIButton) {
+//        self.navigationController?.dismiss(animated: true, completion: nil)
+        action?()
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ ReusablePopup/ReusablePopup/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ReusablePopup
+//
+//  Created by bhavesh on 29/05/21.
+//  Copyright © 2021 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func presentPopup(_ sender: UIButton) {
+        presentPopup("Hello", message: "we showed reusable popup", actionName: "OK") {
+            print("This is the action")
+        }
+    }
+
+
+    func presentPopup(_ title: String = "", message: String = "", actionName: String = "OK", action: (()->Void)?){
+        let popup = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: ReusablePopupViewController.storyboardIdentifier) as! ReusablePopupViewController
+        popup.modalPresentationStyle = .overCurrentContext
+        popup.modalTransitionStyle = .crossDissolve
+        popup.title = title
+        popup.message = message
+        popup.actionTitle = actionName
+        popup.action = action
+        self.navigationController?.present(popup, animated: true, completion: nil)
+    }
+}
